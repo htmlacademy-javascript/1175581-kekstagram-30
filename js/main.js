fd00e9cb9885774b7da46147c51f60be65a66f2e
Karen <kste.prog@gmail.com>
2023-10-24 17:30:12 +0300

    вынес константы в отдельный модуль

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Точка входа
 
 import { createPhotoPostArray } from './data.js';
-
+import { MAX_PHOTO_POSTS } from './constants.js';
 //Создаем массив обьектов с описанием фотографии
-console.log(createPhotoPostArray());
+console.log(createPhotoPostArray(MAX_PHOTO_POSTS));
